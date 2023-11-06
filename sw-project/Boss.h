7aa8fd4e050ec631853cfbcbd477cfc1cee07198
Famec <68315380+Famec7@users.noreply.github.com>
2023-11-06 11:29:05 +0900

    보스 랜덤 움직임 구현
    범위와 최소 이동 거리를 이용한 좌우 랜덤 이동

--- sw-project/Boss.h
+++ sw-project/Boss.h
@@ -1,6 +1,8 @@
 #pragma once
+#include "gameInfo.h"
 #include <string>
 #include <iostream>
+#include <time.h>
 
 #define BOSS_SIZE_Y 10
 #define BOSS_SIZE_X 10
@@ -78,14 +80,14 @@ int bossModel[][BOSS_SIZE_Y][BOSS_SIZE_X] = {
 COORD curBossPos = { BOSS_ORIGIN_X, BOSS_ORIGIN_Y };
 
 int curBossPhase = 0;	// 0이 1번째 페이즈
-int curBossHp = 30;
-int bossSpeed = 100;
-std::string bossHpString = "coding/hagisilda/guichanda/";
+int bossSpeed = 200;
+std::string bossHpString = "coding/hagisilda/guichanda/aaaaaaaaaaaaaaaaa";
+int curBossHp = bossHpString.length();
 /****************보스 스탯 초기화 함수*********************/
 void BossInit()
 {
 	curBossPhase = 0;
-	curBossHp = 30;
+	curBossHp = bossHpString.length();
 	curBossPos = { BOSS_ORIGIN_X, BOSS_ORIGIN_Y };
 }
 /****************보스 모델을 띄우는 함수*********************/
@@ -139,13 +141,15 @@ void DeleteBossModel(int bossModel[][10])
 /****************보스 HP UI를 띄우는 함수*********************/
 void ShowBossHpUI()
 {
-	SetCurrentCursorPos(BOSS_ORIGIN_X, BOSS_ORIGIN_Y - 1);
-	for (int i = 0; i < 10; i++)
+	//보스의 원래 위치보다 한칸 위에 표시
+	SetCurrentCursorPos(BOSS_ORIGIN_X - bossHpString.length() / 2 + BOSS_SIZE_X, BOSS_ORIGIN_Y - 1);
+	for (int i = 0; i < curBossHp; i++)
 		std::cout << bossHpString[i];
 }
 /****************보스 체력 한칸 줄이는 함수*********************/
 void BossLifeDecrease()
 {
+	// 현재 체력 한칸 줄이고 다시 UI에 표시
 	curBossHp--;
 	ShowBossHpUI();
 }
@@ -164,9 +168,42 @@ void BossShiftRight()
 	ShowBossModel(bossModel[curBossPhase]);
 }
 /****************보스의 전체적인 움직임*********************/
-void BossMove()
+void BossRandomMove()
 {
-	;
+	srand(time(NULL));
+	int direction = rand() % 2;	// 방향 정하기 0 : 오른쪽, 1 : 왼쪽
+	int minDistance = 3;	// 최소 이동거리
+	int range = 10;	// 이동 범위
+	if (direction == 0)
+	{
+		int right = rand() % range + minDistance;
+		for (int i = 0; i < right; i++)
+		{
+			BossShiftRight();
+			Sleep(bossSpeed);
+		}
+		int left = rand() % range + minDistance;
+		for (int i = 0; i < left; i++)
+		{
+			BossShiftLeft();
+			Sleep(bossSpeed);
+		}
+	}
+	else
+	{
+		int left = rand() % range + minDistance;
+		for (int i = 0; i < left; i++)
+		{
+			BossShiftLeft();
+			Sleep(bossSpeed);
+		}
+		int right = rand() % range + minDistance;
+		for (int i = 0; i < right; i++)
+		{
+			BossShiftRight();
+			Sleep(bossSpeed);
+		}
+	}
 }
 /****************보스 충돌 함수*********************/
 int BossDetectedCollision(int posX, int posY)
