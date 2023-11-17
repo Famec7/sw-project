ceb81adaea894c34d4c27a90cce5523eeb1b4b0a
gwichana <100696866+gwichana@users.noreply.github.com>
2023-11-18 03:08:06 +0900

    Update player
    플레이어 피격 구현
    피격 시 빨간색으로 변함
    다른 짜잘한 거+

--- SoftwareDesignFoundation/player.cpp
+++ SoftwareDesignFoundation/player.cpp
@@ -1,12 +1,14 @@
 #include "player.h"
+#include "Bullet.h"
 
+double imHit = 0;
 int HP = MAX_HP;					//HP 초기화
 double shield = MAX_SHIELD;		//실드 초기화
 COORD playerCurPos;
 
 int countTime1, countTime2;
 
-int bulletNum = 3;			//초알의 개수
+int bulletNum = 1;			//초알의 개수
 
 int isShield_Flag = 0;		//실드가 사용중인가 0 == 미사용, 1 == 사용중
 
@@ -26,12 +28,13 @@ char hpText[] = { 'P', 'l', 'a', 'y', 'e', 'r',
 char shieldText[] = { 's', 'h', 'i', 'e', 'l', 'd' };
 
 void playerInit() {
-	playerCurPos.X = 20;
-	playerCurPos.Y = 20;
+	playerCurPos.X = 45;
+	playerCurPos.Y = 35;
 }
 void PlayerShowModel() {
 	SetCurrentCursorPos(playerCurPos.X, playerCurPos.Y);
 	COORD curPos = GetCurrentCursorPos();
+	if (imHit != 0)playerColor = 4;
 	SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), playerColor);	//gray
 	int x, y;
 	int arrCurX = (curPos.X - GBOARD_ORIGIN_X) / 2;
@@ -71,14 +74,17 @@ int PlayerDetectedCollision(int x, int y) {
 			if (isShield_Flag == 0) {
 				if (playerModel[y][x] != 0 && gameBoardInfo[arrY + y][arrX + x] == 4) {
 					HP--;
+					imHit = 1;
+					PlayerDeleteModel();
+					PlayerShowModel();
 					return 1;	//총알 충돌
 				}
-				else if (playerModel[y][x] != 0 && gameBoardInfo[arrY + y][arrX + x] == 1)return 0;	//벽 충돌
-				else if (playerModel[y][x] != 0 && gameBoardInfo[arrY + y][arrX + x] == 3)return 0;	//적 충돌
-				else if (playerModel[y][x] != 0 && gameBoardInfo[arrY + y][arrX + x] == 5)return 0;	//아이템 충돌
-				else if (playerModel[y][x] != 0 && gameBoardInfo[arrY + y][arrX + x] == 6)return 0;	//아이템 충돌
-				else if (playerModel[y][x] != 0 && gameBoardInfo[arrY + y][arrX + x] == 7)return 0;	//보스 충돌
 			}
+			if (playerModel[y][x] != 0 && gameBoardInfo[arrY + y][arrX + x] == 1)return 0;	//벽 충돌
+			else if (playerModel[y][x] != 0 && gameBoardInfo[arrY + y][arrX + x] == 3)return 0;	//적 충돌
+			else if (playerModel[y][x] != 0 && gameBoardInfo[arrY + y][arrX + x] == 5)return 0;	//아이템 충돌
+			else if (playerModel[y][x] != 0 && gameBoardInfo[arrY + y][arrX + x] == 6)return 0;	//아이템 충돌
+			else if (playerModel[y][x] != 0 && gameBoardInfo[arrY + y][arrX + x] == 7)return 0;	//보스 충돌
 		}
 	}
 	return 1;
@@ -135,6 +141,7 @@ void PlayerStatOutput() {
 	}
 	SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), 7);
 }
+/*
 void Attack(int input) {
 	COORD pos = playerCurPos;
 	if (input == 97) {
@@ -189,41 +196,63 @@ void Attack(int input) {
 			gameBoardInfo[arrCurY][arrCurX + 4] = input;
 		}
 	}
-
 }
-void ProcessKeyInput() {	//GetAsyncKeyState
-	int key;
-	for (int i = 0; i < 10; i++) {
-		if (_kbhit() != 0) {
-			key = _getch();
-			switch (key) {
-			case 75:
-				PlayerShiftLeft();
-				break;
-			case 77:
-				PlayerShiftRight();
-				break;
-			case 72:
-				PlayerShiftUp();
-				break;
-			case 80:
-				PlayerShiftDown();
-				break;
-			case 13:		//enter
-				UseShield();
-				break;
-			default:
-				Attack(key);
-				break;
+*/
+void ProcessKeyInput() { // GetAsyncKeyState
+	if (_kbhit() == 0)
+		return;
+
+	if (GetAsyncKeyState(VK_LEFT)) {
+		PlayerShiftLeft();
+	}
+	if (GetAsyncKeyState(VK_RIGHT)) {
+		PlayerShiftRight();
+	}
+	if (GetAsyncKeyState(VK_DOWN)) {
+		PlayerShiftDown();
+	}
+	if (GetAsyncKeyState(VK_UP)) {
+		PlayerShiftUp();
+	}
+	if (GetAsyncKeyState(VK_RETURN))
+		UseShield();
+
+	for (int i = 0; i <= 'z' - 'a'; i++) {
+		if (GetAsyncKeyState(0x41 + i)) {
+			if (bulletNum == 1) {
+				MakeBullet(playerCurPos.X + 3, playerCurPos.Y - 2, 'a' + i, 0.1f);
+			}
+			else if (bulletNum == 2) {
+				MakeBullet(playerCurPos.X + 3, playerCurPos.Y - 2, 'a' + i, 0.1f);
+				MakeBullet(playerCurPos.X + 5, playerCurPos.Y - 2, 'a' + i, 0.1f);
+			}
+			else if (bulletNum == 3) {
+				MakeBullet(playerCurPos.X + 1, playerCurPos.Y - 2, 'a' + i, 0.1f);
+				MakeBullet(playerCurPos.X + 3, playerCurPos.Y - 2, 'a' + i, 0.1f);
+				MakeBullet(playerCurPos.X + 5, playerCurPos.Y - 2, 'a' + i, 0.1f);
+			}
+			else if (bulletNum == 4) {
+				MakeBullet(playerCurPos.X + 1, playerCurPos.Y - 2, 'a' + i, 0.1f);
+				MakeBullet(playerCurPos.X + 3, playerCurPos.Y - 2, 'a' + i, 0.1f);
+				MakeBullet(playerCurPos.X + 5, playerCurPos.Y - 2, 'a' + i, 0.1f);
+				MakeBullet(playerCurPos.X + 7, playerCurPos.Y - 2, 'a' + i, 0.1f);
+			}
+			else if (bulletNum == 5) {
+				MakeBullet(playerCurPos.X - 1, playerCurPos.Y - 2, 'a' + i, 0.1f);
+				MakeBullet(playerCurPos.X + 1, playerCurPos.Y - 2, 'a' + i, 0.1f);
+				MakeBullet(playerCurPos.X + 3, playerCurPos.Y - 2, 'a' + i, 0.1f);
+				MakeBullet(playerCurPos.X + 5, playerCurPos.Y - 2, 'a' + i, 0.1f);
+				MakeBullet(playerCurPos.X + 7, playerCurPos.Y - 2, 'a' + i, 0.1f);
 			}
 		}
-		/*Sleep(25);*/
 	}
 }
 void UseShield() {
 	if (shield >= 6.0f) {
 		isShield_Flag = 1;		//실드 사용중
 		playerColor = 9;		//플레이어의 색은 파란색
+		PlayerDeleteModel();
+		PlayerShowModel();
 	}
 }
 void ManageShield() {
@@ -241,6 +270,7 @@ void ManageShield() {
 		if (shield >= MAX_SHIELD)shield = MAX_SHIELD;
 	}
 }
+/*
 void ManageGameboard() {
 	for (int i = 0; i < GBOARD_HEIGHT; i++) {
 		for (int j = 0; j < GBOARD_WIDTH; j++) {
@@ -266,6 +296,23 @@ void ManageGameboard() {
 			}
 		}
 	}
+}
+*/
+void PlayerUpdate() {
+	PlayerDetectedCollision(playerCurPos.X, playerCurPos.Y - 1);
+	if (imHit <= 0) {
+		imHit = 0;
+		if (isShield_Flag == 0) {
+			playerColor = 7;
+		}
+	}
+	else {
+		imHit -= Time.deltaTime;
+	}
+	//PlayerDetectedCollision(playerCurPos.X - 2, playerCurPos.Y);
+	//PlayerDetectedCollision(playerCurPos.X + 2, playerCurPos.Y);
+	//PlayerDetectedCollision(playerCurPos.X, playerCurPos.Y + 1);
+	//PlayerDetectedCollision(playerCurPos.X, playerCurPos.Y - 1);
 }
 int IsGameOver() {
 	if (HP <= 0)return 1;
