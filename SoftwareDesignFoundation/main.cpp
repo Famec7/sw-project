de515a4969c64939aa5897e303fcb3d5ab511276
이주헌 <43901448+dlwngjs@users.noreply.github.com>
2023-11-19 05:40:17 +0900

    item_test
    아이템 생성 추가

--- SoftwareDesignFoundation/main.cpp
+++ SoftwareDesignFoundation/main.cpp
@@ -1,9 +1,35 @@
 #include "Boss.h"
 #include "Bullet.h"
 #include "player.h"
+#include "Item.h"
 
+int main() {
+	srand(time(NULL));
+	int key;
+	COORD pos;
+	ItemInit();
+	while (1) {
+		if (!_kbhit()) {
+			key = _getch();
+			if (key == 'i') {
+				pos.X = rand() % 40 + 8;
+				pos.Y = rand() % 40 + 3;
+				DropItem(pos);
+			}
+			if (key == 'q') {
+				break;
+			}
+		}
+	}
+	getchar();
+
+	return 0;
+}
+
+/*
 int main()
 {
+	srand(time(NULL));
 	RemoveCursor();
 	SetCurrentCursorPos(GBOARD_ORIGIN_X, GBOARD_ORIGIN_Y);
 	system("mode con:cols=100 lines=50");
@@ -33,3 +59,4 @@ int main()
 
 	return 0;
 }
+*/
