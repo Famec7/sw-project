2ab73621f542999ef290188a5644f9092a21dc5d
gwichana <100696866+gwichana@users.noreply.github.com>
2023-11-25 20:24:12 +0900

    플레이어 모델 변경, 실드 색 수정
    플레이어 모델 변경, 실드 색 수정

--- SoftwareDesignFoundation/player.cpp
+++ SoftwareDesignFoundation/player.cpp
@@ -2,29 +2,22 @@
 #include "Bullet.h"
 
 double imHit = 0;
+int CantControl = 0;
+
 int HP = MAX_HP;					//HP 초기화
 double shield = MAX_SHIELD;		//실드 초기화
+//double playerSpeed = 0.5;
+
 COORD playerCurPos;
-
 int countTime1, countTime2;
-
 int bulletNum = 1;			//초알의 개수
+int isShield_Flag = 0; 
+int playerColor = 7;
 
-int isShield_Flag = 0;		//실드가 사용중인가 0 == 미사용, 1 == 사용중
+int playerModel[PLAYER_HEIGHT][PLAYER_WIDTH] = {{2}, {2}};
 
-int playerColor = 7;		//플레이어의 색 7 == gray, 9 == 파란색(실드), 4 == 빨간색(맞았을때)
-int playerModel[5][5] = {		//충돌판정은 머리부분 3*3만
-	{0, 2, 2, 2, 0},
-	{0, 2, 2, 2, 0},
-	{2, 2, 2, 2, 2},
-	{0, 2, 2, 2, 0},
-	{0, 2, 0, 2, 0}
-};
-//화면에 Player 상태 출력
-char hpText[] = { 'P', 'l', 'a', 'y', 'e', 'r',
-				'h', 'p',
-				'i', 's',
-				'f', 'u', 'l', 'l' };
+char hpText[] = { 'P', 'l', 'a', 'y', 'e', 'r', 'h',
+				 'p', 'i', 's', 'f', 'u', 'l', 'l' };
 char shieldText[] = { 's', 'h', 'i', 'e', 'l', 'd' };
 
 void playerInit() {
@@ -34,21 +27,26 @@ void playerInit() {
 void PlayerShowModel() {
 	SetCurrentCursorPos(playerCurPos.X, playerCurPos.Y);
 	COORD curPos = GetCurrentCursorPos();
-	if (imHit != 0)playerColor = 4;
+	if (imHit != 0 && isShield_Flag == 0)playerColor = 4;
 	SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), playerColor);	//gray
 	int x, y;
 	int arrCurX = (curPos.X - GBOARD_ORIGIN_X) / 2;
 	int arrCurY = (curPos.Y - GBOARD_ORIGIN_Y);
-	for (y = 0; y < 5; y++) {
-		for (x = 0; x < 5; x++) {
+	for (y = 0; y < PLAYER_HEIGHT; y++) {
+		for (x = 0; x < PLAYER_WIDTH; x++) {
 			if (playerModel[y][x] == 2) {
 				SetCurrentCursorPos(curPos.X + (x * 2), curPos.Y + y);
-				printf("■");
+				if (y == 0) {
+					printf("●");
+				}
+				else if (y == 1) {
+					printf("▲");
+				}
 				gameBoardInfo[arrCurY + y][arrCurX + x] = 2;
 			}
 		}
 	}
-	SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), 7);	//커서 색 초기화
+	SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), 7);
 }
 void PlayerDeleteModel() {
 	SetCurrentCursorPos(playerCurPos.X, playerCurPos.Y);
@@ -56,8 +54,8 @@ void PlayerDeleteModel() {
 	int x, y;
 	int arrCurX = (curPos.X - GBOARD_ORIGIN_X) / 2;
 	int arrCurY = (curPos.Y - GBOARD_ORIGIN_Y);
-	for (y = 0; y < 5; y++) {
-		for (x = 0; x < 5; x++) {
+	for (y = 0; y < PLAYER_HEIGHT; y++) {
+		for (x = 0; x < PLAYER_WIDTH; x++) {
 			SetCurrentCursorPos(curPos.X + (x * 2), curPos.Y + y);
 			if (playerModel[y][x] == 2) {
 				printf("  ");
@@ -69,8 +67,8 @@ void PlayerDeleteModel() {
 int PlayerDetectedCollision(int x, int y) {
 	int arrX = (x - GBOARD_ORIGIN_X) / 2;
 	int arrY = y - GBOARD_ORIGIN_Y;
-	for (int y = 0; y < 5; y++) {
-		for (int x = 0; x < 5; x++) {
+	for (int y = 0; y < PLAYER_HEIGHT; y++) {
+		for (int x = 0; x < PLAYER_WIDTH; x++) {
 			if (isShield_Flag == 0) {
 				if (playerModel[y][x] != 0 && gameBoardInfo[arrY + y][arrX + x] == 4) {
 					HP--;
@@ -90,7 +88,8 @@ int PlayerDetectedCollision(int x, int y) {
 	return 1;
 }
 int PlayerShiftLeft() {
-	if (!PlayerDetectedCollision(playerCurPos.X - 2, playerCurPos.Y))return 0;
+	if (!PlayerDetectedCollision(playerCurPos.X - 2, playerCurPos.Y))
+		return 0;
 	PlayerDeleteModel();
 	playerCurPos.X -= 2;
 	SetCurrentCursorPos(playerCurPos.X, playerCurPos.Y);
@@ -98,7 +97,8 @@ int PlayerShiftLeft() {
 	return 1;
 }
 int PlayerShiftRight() {
-	if (!PlayerDetectedCollision(playerCurPos.X + 2, playerCurPos.Y))return 0;
+	if (!PlayerDetectedCollision(playerCurPos.X + 2, playerCurPos.Y))
+		return 0;
 	PlayerDeleteModel();
 	playerCurPos.X += 2;
 	SetCurrentCursorPos(playerCurPos.X, playerCurPos.Y);
@@ -106,7 +106,8 @@ int PlayerShiftRight() {
 	return 1;
 }
 int PlayerShiftUp() {
-	if (!PlayerDetectedCollision(playerCurPos.X, playerCurPos.Y - 1))return 0;
+	if (!PlayerDetectedCollision(playerCurPos.X, playerCurPos.Y - 1))
+		return 0;
 	PlayerDeleteModel();
 	playerCurPos.Y -= 1;
 	SetCurrentCursorPos(playerCurPos.X, playerCurPos.Y);
@@ -114,7 +115,8 @@ int PlayerShiftUp() {
 	return 1;
 }
 int PlayerShiftDown() {
-	if (!PlayerDetectedCollision(playerCurPos.X, playerCurPos.Y + 1))return 0;
+	if (!PlayerDetectedCollision(playerCurPos.X, playerCurPos.Y + 1))
+		return 0;
 	PlayerDeleteModel();
 	playerCurPos.Y += 1;
 	SetCurrentCursorPos(playerCurPos.X, playerCurPos.Y);
@@ -127,8 +129,10 @@ void PlayerStatOutput() {
 	SetCurrentCursorPos(10, 45);
 	SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), 4);
 	for (int i = 0; i < 14; i++) {
-		if (i == HP)break;	//HP가 2이면 pl만 출력, i가 2이면 a를 출력하게되, 이를 못하게 막음
-		if (i == 6 || i == 8 || i == 10) printf(" ");
+		if (i == HP)
+			break;
+		if (i == 6 || i == 8 || i == 10)
+			printf(" ");
 		printf("%c", hpText[i]);
 	}
 	SetCurrentCursorPos(10, 46);
@@ -136,72 +140,17 @@ void PlayerStatOutput() {
 	SetCurrentCursorPos(10, 46);
 	SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), 9);
 	for (int i = 0; i < 6; i++) {
-		if (i == (int)shield)break;
+		if (i == (int)shield)
+			break;
 		printf("%c", shieldText[i]);
 	}
 	SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), 7);
 }
-/*
-void Attack(int input) {
-	COORD pos = playerCurPos;
-	if (input == 97) {
-		if (bulletNum == 1) {
-			pos.Y = pos.Y - 1;
-			pos.X = pos.X + 4;
-			int arrCurX, arrCurY;
-			arrCurX = (pos.X - GBOARD_ORIGIN_X) / 2;
-			arrCurY = (pos.Y - GBOARD_ORIGIN_Y);
-			gameBoardInfo[arrCurY][arrCurX] = input;
-		}
-		else if (bulletNum == 2) {
-			pos.Y = pos.Y - 1;
-			pos.X = pos.X + 4;
-			int arrCurX, arrCurY;
-			arrCurX = (pos.X - GBOARD_ORIGIN_X) / 2;
-			arrCurY = (pos.Y - GBOARD_ORIGIN_Y);
-			gameBoardInfo[arrCurY][arrCurX] = input;
-			gameBoardInfo[arrCurY][arrCurX + 1] = input;
-		}
-		else if (bulletNum == 3) {
-			pos.Y = pos.Y - 1;
-			pos.X = pos.X + 4;
-			int arrCurX, arrCurY;
-			arrCurX = (pos.X) / 2;
-			arrCurY = (pos.Y);
-			gameBoardInfo[arrCurY][arrCurX - 2] = input;
-			gameBoardInfo[arrCurY][arrCurX] = input;
-			gameBoardInfo[arrCurY][arrCurX + 2] = input;
-		}
-		else if (bulletNum == 4) {
-			pos.Y = pos.Y - 1;
-			pos.X = pos.X + 4;
-			int arrCurX, arrCurY;
-			arrCurX = (pos.X - GBOARD_ORIGIN_X) / 2;
-			arrCurY = (pos.Y - GBOARD_ORIGIN_Y);
-			gameBoardInfo[arrCurY][arrCurX - 1] = input;
-			gameBoardInfo[arrCurY][arrCurX] = input;
-			gameBoardInfo[arrCurY][arrCurX + 1] = input;
-			gameBoardInfo[arrCurY][arrCurX + 2] = input;
-		}
-		else if (bulletNum == 5) {
-			pos.Y = pos.Y - 1;
-			pos.X = pos.X + 4;
-			int arrCurX, arrCurY;
-			arrCurX = (pos.X - GBOARD_ORIGIN_X) / 2;
-			arrCurY = (pos.Y - GBOARD_ORIGIN_Y);
-			gameBoardInfo[arrCurY][arrCurX - 4] = input;
-			gameBoardInfo[arrCurY][arrCurX - 2] = input;
-			gameBoardInfo[arrCurY][arrCurX] = input;
-			gameBoardInfo[arrCurY][arrCurX + 2] = input;
-			gameBoardInfo[arrCurY][arrCurX + 4] = input;
-		}
-	}
-}
-*/
 void ProcessKeyInput() { // GetAsyncKeyState
+	//if (playerSpeed > 0)return;
+	if (CantControl == 1)return;
 	if (_kbhit() == 0)
 		return;
-
 	if (GetAsyncKeyState(VK_LEFT)) {
 		PlayerShiftLeft();
 	}
@@ -220,32 +169,34 @@ void ProcessKeyInput() { // GetAsyncKeyState
 	for (int i = 0; i <= 'z' - 'a'; i++) {
 		if (GetAsyncKeyState(0x41 + i)) {
 			if (bulletNum == 1) {
-				MakeBullet(playerCurPos.X + 3, playerCurPos.Y - 2, 'a' + i, 0.1f);
+				MakeBullet(playerCurPos.X, playerCurPos.Y - 2, 'a' + i, 0.01f);
 			}
 			else if (bulletNum == 2) {
-				MakeBullet(playerCurPos.X + 3, playerCurPos.Y - 2, 'a' + i, 0.1f);
-				MakeBullet(playerCurPos.X + 5, playerCurPos.Y - 2, 'a' + i, 0.1f);
+				MakeBullet(playerCurPos.X + 1, playerCurPos.Y - 2, 'a' + i, 0.01f);
+				MakeBullet(playerCurPos.X - 1, playerCurPos.Y - 2, 'a' + i, 0.01f);
 			}
 			else if (bulletNum == 3) {
-				MakeBullet(playerCurPos.X + 1, playerCurPos.Y - 2, 'a' + i, 0.1f);
-				MakeBullet(playerCurPos.X + 3, playerCurPos.Y - 2, 'a' + i, 0.1f);
-				MakeBullet(playerCurPos.X + 5, playerCurPos.Y - 2, 'a' + i, 0.1f);
+				MakeBullet(playerCurPos.X + 2, playerCurPos.Y - 2, 'a' + i, 0.01f);
+				MakeBullet(playerCurPos.X, playerCurPos.Y - 2, 'a' + i, 0.01f);
+				MakeBullet(playerCurPos.X - 2, playerCurPos.Y - 2, 'a' + i, 0.01f);
 			}
 			else if (bulletNum == 4) {
-				MakeBullet(playerCurPos.X + 1, playerCurPos.Y - 2, 'a' + i, 0.1f);
-				MakeBullet(playerCurPos.X + 3, playerCurPos.Y - 2, 'a' + i, 0.1f);
-				MakeBullet(playerCurPos.X + 5, playerCurPos.Y - 2, 'a' + i, 0.1f);
-				MakeBullet(playerCurPos.X + 7, playerCurPos.Y - 2, 'a' + i, 0.1f);
+				MakeBullet(playerCurPos.X + 3, playerCurPos.Y - 2, 'a' + i, 0.01f);
+				MakeBullet(playerCurPos.X + 1, playerCurPos.Y - 2, 'a' + i, 0.01f);
+				MakeBullet(playerCurPos.X - 1, playerCurPos.Y - 2, 'a' + i, 0.01f);
+				MakeBullet(playerCurPos.X - 3, playerCurPos.Y - 2, 'a' + i, 0.01f);
 			}
 			else if (bulletNum == 5) {
-				MakeBullet(playerCurPos.X - 1, playerCurPos.Y - 2, 'a' + i, 0.1f);
-				MakeBullet(playerCurPos.X + 1, playerCurPos.Y - 2, 'a' + i, 0.1f);
-				MakeBullet(playerCurPos.X + 3, playerCurPos.Y - 2, 'a' + i, 0.1f);
-				MakeBullet(playerCurPos.X + 5, playerCurPos.Y - 2, 'a' + i, 0.1f);
-				MakeBullet(playerCurPos.X + 7, playerCurPos.Y - 2, 'a' + i, 0.1f);
+				MakeBullet(playerCurPos.X + 4, playerCurPos.Y - 2, 'a' + i, 0.01f);
+				MakeBullet(playerCurPos.X + 2, playerCurPos.Y - 2, 'a' + i, 0.01f);
+				MakeBullet(playerCurPos.X, playerCurPos.Y - 2, 'a' + i, 0.01f);
+				MakeBullet(playerCurPos.X - 2, playerCurPos.Y - 2, 'a' + i, 0.01f);
+				MakeBullet(playerCurPos.X - 4, playerCurPos.Y - 2, 'a' + i, 0.01f);
 			}
 		}
 	}
+	//playerSpeed += 0.5;
+	Sleep(50);
 }
 void UseShield() {
 	if (shield >= 6.0f) {
@@ -265,56 +216,33 @@ void ManageShield() {
 		}
 	}
 	else {
-		if (shield >= MAX_SHIELD)return;
+		if (shield >= MAX_SHIELD)
+			return;
 		shield += Time.deltaTime;
-		if (shield >= MAX_SHIELD)shield = MAX_SHIELD;
+		if (shield >= MAX_SHIELD)
+			shield = MAX_SHIELD;
 	}
 }
-/*
-void ManageGameboard() {
-	for (int i = 0; i < GBOARD_HEIGHT; i++) {
-		for (int j = 0; j < GBOARD_WIDTH; j++) {
-			if (gameBoardInfo[i][j] == 97) {
-				SetCurrentCursorPos(j * 2, i);
-				printf("a");
-				//gameBoardInfo[i][j] = 0;
-				//SetCurrentCursorPos(i + GBOARD_ORIGIN_Y, j + GBOARD_ORIGIN_X);
-				//printf(" ");
-				//gameBoardInfo[i - 1][j] = 97;
-				//SetCurrentCursorPos(i + GBOARD_ORIGIN_Y - 1, j + GBOARD_ORIGIN_X);
-				//printf("a");
-			}
-			if (gameBoardInfo[i][j] == 2) {
-				SetCurrentCursorPos(j * 2, i);
-				//printf("■");
-				//gameBoardInfo[i][j] = 0;
-				//SetCurrentCursorPos(i + GBOARD_ORIGIN_Y, j + GBOARD_ORIGIN_X);
-				//printf(" ");
-				//gameBoardInfo[i - 1][j] = 97;
-				//SetCurrentCursorPos(i + GBOARD_ORIGIN_Y - 1, j + GBOARD_ORIGIN_X);
-				//printf("a");
-			}
-		}
-	}
-}
-*/
 void PlayerUpdate() {
 	PlayerDetectedCollision(playerCurPos.X, playerCurPos.Y - 1);
+	PlayerDeleteModel();
+	PlayerShowModel();
 	if (imHit <= 0) {
 		imHit = 0;
 		if (isShield_Flag == 0) {
 			playerColor = 7;
 		}
+		if (isShield_Flag == 1) {
+			playerColor = 9;
+		}
 	}
 	else {
 		imHit -= Time.deltaTime;
 	}
-	//PlayerDetectedCollision(playerCurPos.X - 2, playerCurPos.Y);
-	//PlayerDetectedCollision(playerCurPos.X + 2, playerCurPos.Y);
-	//PlayerDetectedCollision(playerCurPos.X, playerCurPos.Y + 1);
-	//PlayerDetectedCollision(playerCurPos.X, playerCurPos.Y - 1);
 }
 int IsGameOver() {
-	if (HP <= 0)return 1;
-	else return 0;
+	if (HP <= 0)
+		return 1;
+	else
+		return 0;
 }
