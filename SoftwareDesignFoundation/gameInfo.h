c52d3037ee9190f5497f4cb1cabb80a003764450
gwichana <100696866+gwichana@users.noreply.github.com>
2023-11-25 20:39:33 +0900

    Update gameInfo.h

--- SoftwareDesignFoundation/gameInfo.h
+++ SoftwareDesignFoundation/gameInfo.h
@@ -4,33 +4,37 @@
 #include <stdio.h>
 #include <time.h>
 
-// °ÔÀÓ ¿ä¼Ò
+// ê²Œì„ ìš”ì†Œ
 #define MAP_BOUNDARY 1
 #define PLAYER 2
 #define ENEMY 3
-#define BULLET 4	// Àû ÃÑ¾Ë
+#define BULLET 4	// ì  ì´ì•Œ
 #define HP_ITEM 5
 #define UPGRADE_ITEM 6
 #define BOSS 7
+#define NORMAL_MOB_1 8
+#define NORMAL_MOB_1 9
+#define NORMAL_MOB_1 10
+#define NORMAL_MOB_1 11
 
-// ¸Ê Å©±â
+// ë§µ í¬ê¸°
 #define GBOARD_HEIGHT 40
 #define GBOARD_WIDTH 40
 
-// ¸Ê À§Ä¡
-#define GBOARD_ORIGIN_X 7
+// ë§µ ìœ„ì¹˜
+#define GBOARD_ORIGIN_X 8
 #define GBOARD_ORIGIN_Y 3
 
 extern int gameBoardInfo[GBOARD_HEIGHT + 1][GBOARD_WIDTH + 2];
 
 struct TimeStruct{
-	double time;	// °ÔÀÓÀÇ ÃÑ ½Ã°£ ÀúÀå
+	double time;	// ê²Œì„ì˜ ì´ ì‹œê°„ ì €ì¥
 	double deltaTime;
 };
 
 extern TimeStruct Time;
 
-//gameInfo¸¦ °»½ÅÇÒ ÇÔ¼ö Ãß°¡?
+//gameInfoë¥¼ ê°±ì‹ í•  í•¨ìˆ˜ ì¶”ê°€?
 void InitTime();
 void UpdateTime();
 void DrawGameBoard(void);
