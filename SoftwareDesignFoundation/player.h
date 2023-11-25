dcc62726270fb5785730f15d053614a85bfd837a
gwichana <100696866+gwichana@users.noreply.github.com>
2023-11-25 20:24:43 +0900

    플레이어 모델, 실드 색

--- SoftwareDesignFoundation/player.h
+++ SoftwareDesignFoundation/player.h
@@ -7,7 +7,16 @@
 #include <stdio.h>
 
 #define MAX_HP 14		//플레이어의 최대 채력		//player hp is full <= 14글자임
-#define MAX_SHIELD	 6	//플레이어의 최대 실드 게이지 //shield <= 6글자임
+#define MAX_SHIELD 6	//플레이어의 최대 실드 게이지 //shield <= 6글자임
+#define MAX_BULLET 5
+
+#define PLAYER_HEIGHT 2
+#define PLAYER_WIDTH 1
+
+extern int HP;
+extern int bulletNum;
+extern int CantControl;
+extern COORD playerCurPos;
 
 void playerInit();
 void PlayerShowModel();
